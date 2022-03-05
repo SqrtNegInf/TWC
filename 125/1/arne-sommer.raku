8f3295e013b909da414559e852fe371604da5339
David Hoekman <sqrtneginf@gmail.com>
2022-03-04 21:04:40 -0800

    challenge 125, tasks 1 & 2, with all task output/error

---
+++ 125/1/arne-sommer.raku
@@ -0,0 +1,34 @@
+#!/usr/bin/env raku
+
+#die 'not ok - disabled for JVM: too slow' if $*VM ~~ /jvm/;
+
+unit sub MAIN (Int $N where $N > 0 = 13);
+
+my $match = False;
+
+my $N2 = $N * $N;
+
+for 1 .. $N2 -> $a
+{
+  for $a .. $N2 -> $b
+  {
+    for $b + 1 .. $N2 -> $c
+    {
+      next unless any($a, $b, $c) == $N;
+
+      my $left  = $a * $a + $b * $b;
+      my $right = $c * $c;
+
+      last if $right > $left;
+	     
+      if $left == $right
+      {
+        say "($a, $b, $c)";
+	$match = True;
+      }
+    }
+  }
+}
+
+say "-1" unless $match;
+
---
+++ 125/1/ulrich-rieke.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict ;
+use warnings ;
+use feature 'say' ;
+
+my $N = $ARGV[ 0 ] // 60;
+my %triples ;
+for my $i (1 .. 100 ) {
+  for my $j (1 .. 100 ) {
+      for my $k (1 .. 100 ) {
+    if ( ($i ** 2) + ( $j ** 2 ) == $k ** 2 ) {
+        if ( ($i == $N) or ( $j == $N ) or ( $k == $N )) {
+          my @sorted = sort { $a <=> $b } ( $i , $j , $k ) ;
+          $triples{join( ', ' ,  @sorted)}++ ;
+        }
+    }
+      }
+  }
+}
+if ( keys %triples ) {
+  map { say '(' . $_ . ')' } keys %triples ;
+}
+else {
+  say -1 ;
+}
