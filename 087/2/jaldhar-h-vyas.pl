e055dec94dcfd25d4be91e85cb5d07e7786474ad
David Hoekman <sqrtneginf@gmail.com>
2022-03-24 21:39:09 -0700

    challenge 087, task 2, with all task output/error

---
+++ 087/2/jaldhar-h-vyas.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+use 5.020;
+use warnings;
+use English qw/ -no_match_vars /;
+
+sub usage {
+    print<<"-USAGE-";
+Usage:
+  $PROGRAM_NAME <file>
+  
+    <file>    a file describing a matrix of 1's and 0's where every line is a row in the matrix.
+-USAGE-
+    exit 0;
+}
+
+sub all {
+    my @array = @_;
+
+    for (@array) {
+        if ($_ != 1) {
+            return undef;
+        }
+    }
+
+    return 1;
+}
+
+my @matrix = (
+[0,0,0,1,0,0],
+[1,1,1,0,0,0],
+[0,0,1,0,0,1],
+[1,1,1,1,1,0],
+[1,1,1,1,1,0],
+);
+my $maxwidth = 0;
+my $maxheight = 0;
+
+for my $m (0 .. scalar @matrix - 1) {
+    for my $n (0 .. scalar @{$matrix[$m]} - 1) {
+        if (@{$matrix[$m]}[$n] == 1) {
+            my $row = $m;
+            my $col = $n;
+            my $left = $n;
+            my $height = 0;
+            my $width = 0;
+            while ($col < scalar @{$matrix[$row]} && @{$matrix[$row]}[$col] == 1) {
+                $width++;
+                $col++;
+            }
+
+            while ($row < scalar @matrix && all(@{$matrix[$row]}[$left .. $left + $width - 1])) {
+                $height++;
+                $row++;
+            }
+
+            if ($height * $width > $maxheight * $maxwidth) {
+                $maxheight = $height;
+                $maxwidth = $width;
+            }
+        }
+    }
+}
+
+if ($maxheight * $maxwidth < 2) {
+    say '0';
+} else {
+    for (0 .. $maxheight - 1) {
+        say q{[ }, "1 " x $maxwidth, q{]};
+    }
+}
---
+++ 087/2/jaldhar-h-vyas.raku
@@ -0,0 +1,54 @@
+#!/usr/bin/env raku
+
+#sub MAIN(
+#    Str $file #= a file describing a matrix of 1's and 0's where every line
+              #= is a row in the matrix.
+#) {
+#    my @matrix;
+#    for $file.IO.lines -> $line {
+#        @matrix.push($line.match(/ (0|1) /, :g));
+#    }
+my @matrix =
+        < 0 0 0 1 0 0 >,
+        < 1 1 1 0 0 0 >,
+        < 0 0 1 0 0 1 >,
+        < 1 1 1 1 1 0 >,
+        < 1 1 1 1 1 0 >;
+
+    my $maxheight = 0;
+    my $maxwidth = 0;
+
+    for 0 ..^ @matrix.elems -> $m {
+        for 0 ..^ @matrix[$m].elems -> $n {
+            if @matrix[$m][$n] == 1 {
+                my $row = $m;
+                my $col = $n;
+                my $left = $n;
+                my $height = 0;
+                my $width = 0;
+                while $col < @matrix[$row].elems && @matrix[$row][$col] == 1 {
+                    $width++;
+                    $col++;
+                }
+
+                while $row < @matrix.elems && @matrix[$row][$left ..^ $left + $width].all == 1 {
+                    $height++;
+                    $row++;
+                }
+
+                if $height * $width > $maxheight * $maxwidth {
+                    $maxheight = $height;
+                    $maxwidth = $width;
+                }
+            }
+        }
+    }
+
+    if $maxheight * $maxwidth < 2 {
+        say '0';
+    } else {
+        for 0 ..^ $maxheight {
+            say q{[ }, "1 " x $maxwidth, q{]};
+        }
+    }
+#}
