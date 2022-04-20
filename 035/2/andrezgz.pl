cdc85cbf14a1f0ab9fb7576d6ed6b258d07bf729
David Hoekman <sqrtneginf@gmail.com>
2022-04-20 07:00:34 -0700

    challenge 035 task 2, with all task output/error

---
+++ 035/2/andrezgz.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+
+# https://perlweeklychallenge.org/blog/perl-weekly-challenge-035/
+# Task #2
+# Write a program to decode binary morse code.
+# Consider how it might be possible to recover from badly formed morse code.
+#
+# a) by splitting the morse code on gaps
+# b) without looking further than one digit ahead
+
+use strict;
+use warnings;
+
+my %morse = (
+    A => '.-',      N => '-.',
+    B => '-...',    O => '---',
+    C => '-.-.',    P => '.--.',
+    D => '-..',     Q => '--.-',
+    E => '.',       R => '.-.',
+    F => '..-.',    S => '...',
+    G => '--.',     T => '-',
+    H => '....',    U => '..-',
+    I => '..',      V => '...-',
+    J => '.---',    W => '.--',
+    K => '-.-',     X => '-..-',
+    L => '.-..',    Y => '-.--',
+    M => '--',      Z => '--..',
+
+    0 => '-----',   5 => '.....',
+    1 => '.----',   6 => '-....',
+    2 => '..---',   7 => '--...',
+    3 => '...--',   8 => '---..',
+    4 => '....-',   9 => '----.',
+);
+
+my %morse_inv = map { $morse{$_} => $_ } keys %morse;
+
+my %morse_bin_inv = (
+    '1'        => \&add_dot   ,  # dot
+    '111'      => \&add_dash  ,  # dash
+    '0'        => sub { }     ,  # intra-character gap
+    '000'      => \&add_char  ,  # character gap
+    '0000000'  => \&add_word  ,  # word gap
+);
+
+my $seq    = '';
+my $token  = '';
+my $output = [ '' ];
+
+my $bmc = '101010001110111011100010101';
+
+foreach my $i (0 .. (length $bmc)-1 ) {
+    my $actual = substr $bmc, $i, 1;
+    my $next = substr $bmc, $i+1, 1;
+    $seq .= $actual;
+    if ($actual ne $next) {
+        $morse_bin_inv{$seq}->();
+        $seq = '';
+    }
+}
+add_char();
+
+print join ' ', @$output;
+exit 0;
+
+
+sub add_dot {
+    $token .= '.';
+}
+
+sub add_dash {
+    $token .= '-';
+}
+sub add_char {
+    $output->[-1] .= $morse_inv{$token};
+    $token = '';
+}
+sub add_word {
+    add_char();
+    push @$output, '';
+}
---
+++ 035/2/yet-ebreo.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+my %m_decode = ( 
+    "489335"  =>"0",
+    "96119"   =>"1",
+    "22391"   =>"2",
+    "5495"    =>"3",
+    "1367"    =>"4",
+    "341"     =>"5",
+    "1877"    =>"6",
+    "7637"    =>"7",
+    "30581"   =>"8",
+    "122333"  =>"9",
+    "23"      =>"A",
+    "469"     =>"B",
+    "1885"    =>"C",
+    "117"     =>"D",
+    "1"       =>"E",
+    "349"     =>"F",
+    "477"     =>"G",
+    "85"      =>"H",
+    "5"       =>"I",
+    "6007"    =>"J",
+    "471"     =>"K",
+    "373"     =>"L",
+    "119"     =>"M",
+    "29"      =>"N",
+    "1911"    =>"O",
+    "1501"    =>"P",
+    "7639"    =>"Q",
+    "93"      =>"R",
+    "21"      =>"S",
+    "7"       =>"T",
+    "87"      =>"U",
+    "343"     =>"V",
+    "375"     =>"W",
+    "1879"    =>"X",
+    "7543"    =>"Y",
+    "1909"    =>"Z"
+);
+
+my $string_to_decode = '101010001110111011100010101';
+
+sub decode {
+    my $string = pop =~
+    #Replace the assigned word gap with @
+    s/0000000/@/gr =~
+    #Replace the assigned char gap with #
+    s/000/#/gr =~
+    #Replace morse code with equivalent char from hash
+    s/[01]+/$m_decode{oct "b$&"}/gre =~
+    #Replace @ with space
+    s/@/ /gr =~
+    #Remove #'s
+    s/#//gr;
+    return $string
+}
+
+say "Decoded String: ".decode($string_to_decode);
