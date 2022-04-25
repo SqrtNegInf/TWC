e74f781c6edf09167d714daa98a631d32ef206f5
David Hoekman <sqrtneginf@gmail.com>
2022-04-24 17:26:57 -0700

    challenge 027 task 2, with all task output/error

---
+++ 027/2/trenton-langer.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+use strict;
+no warnings;
+
+tie my $x, 'HistoricalScalar';
+$x = 10;
+$x = 20;
+$x -= 5;
+untie $x;
+
+package HistoricalScalar;
+sub TIESCALAR # obj creation (tie $scalar, 'NAME', args)
+{
+    my $class = shift;
+
+    my $self;
+    $self->{_ARRAY} = [];
+        
+    return bless $self, $class;
+}
+sub FETCH # retrieve values
+{
+    my $self = shift;
+    return $self->{_ARRAY}->[-1];
+}
+sub STORE # stores values
+{
+    my $self = shift;
+    my $input = shift;     
+    push @{$self->{_ARRAY}}, $input;
+}
+sub UNTIE # indicate no longer need tied variable (untie $@%var;)
+{
+    my $self = shift;
+    print "History:\n";
+    foreach my $val (@{$self->{_ARRAY}})
+    {
+        print "\t->\t$val\n";
+    }
+}
+sub DESTROY # clean up (undef $@%var;)
+{
+    my $self = shift;
+}
+
+
