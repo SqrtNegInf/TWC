7f28b1c5e6de54381eafc20d53fdd1fc8901272f
David Hoekman <sqrtneginf@gmail.com>
2022-07-07 07:07:29 -0700

    clean-up for output stability, 094/1/wanderdoc.pl

--- 094/1/wanderdoc.pl
+++ 094/1/wanderdoc.pl
@@ -1,25 +1,23 @@
 #!/usr/bin/env perl
+
 use strict;
-use warnings FATAL => qw(all);
+use warnings;
+use feature 'say';
 
 # original tries to produce stable output, but fails, see if I can manage it...
-print join "\n", map { join ', ', sort { $a cmp $b } @$_ } group_anagrams("opt", "bat", "saw", "tab", "pot", "top", "was");
+say join "\n", sort map { join ', ', sort { $a cmp $b } @$_ } group_anagrams("opt", "bat", "saw", "tab", "pot", "top", "was");
 
 #my @groups = group_anagrams("opt", "bat", "saw", "tab", "pot", "top", "was");
-##print join(", ", @$_), $/ for sort { $a cmp  $b } @groups; 
-#print join "\n", map { sort { $a cmp  $b } $_ } map { join ', ', sort { $a cmp $b } @$_ } @groups; 
+#print join(", ", @$_), $/ for sort { $a cmp  $b } @groups; 
 
-sub group_anagrams
-{
+sub group_anagrams {
      my @strings = @_;
-
      my %anagrams;
-     push @{$anagrams{standard($_)}}, $_ for sort @strings;
+     push @{$anagrams{standard($_)}}, $_ for @strings;
      return sort values %anagrams;
 }
 
-sub standard
-{
-     my $string = $_[0];
+sub standard {
+     my $string = shift;
      return join('',sort {$a cmp $b} split(//,$string));
 }
