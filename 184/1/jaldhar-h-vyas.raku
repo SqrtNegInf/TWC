dbfe89b9e23ef18c67c60a4d64fe7ef6e4d10b82
David Hoekman <sqrtneginf@gmail.com>
2022-10-03 15:56:07 -0700

    mostly just raw code after tweaks

---
+++ 184/1/jaldhar-h-vyas.raku
@@ -0,0 +1,9 @@
+#!/usr/bin/env raku
+
+sub MAIN (
+    *@list
+) {
+    my $seq = '00';
+    my @output = @list.map({ $_.subst(/^../, $seq++)});
+    @output.join(q{ }).say;
+}
---
+++ 184/1/james-raspass.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use v5.36;
+
+srand  1;
+
+my @default = ( [qw/ab1234 cd5678 ef1342/], [qw/pq1122 rs3334/] );
+my @input   = @ARGV ? @ARGV : $default[rand @default]->@*;
+
+my $i      = '00';
+my @output = map s/../$i++/er, @input;
+
+say do { local $" = ', '; "Input:  @input\nOutput: @output" };
