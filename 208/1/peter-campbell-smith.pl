443777a36225f86f2c5b52c8db20e9ac9e979e63
David Hoekman <sqrtneginf@gmail.com>
2023-03-20 05:38:27 -0700

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 208/1/peter-campbell-smith.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+srand 1;
+
+use v5.28;
+use utf8;
+use warnings;
+
+my ($j, @list1, @list2);
+
+min_index_sum(["Perl", "Raku", "Love"], ["Raku", "Perl", "Hate"]);
+min_index_sum(['A', 'B', 'C'], ['D', 'E', 'F']);
+min_index_sum(['A', 'B', 'C'], ['C', 'A', 'B']);
+
+# make a longer example - 100 random letters in each list
+for $j (0 .. 99) {
+	$list1[$j] = chr(ord('a') + int(rand(26)));
+	$list2[$j] = chr(ord('a') + int(rand(26)));
+}	
+min_index_sum(\@list1, \@list2);
+	
+sub min_index_sum {
+
+	my (@list1, @list2, $j, %index, $least, $rubric1, $sum, $index1, $index2);
+	
+	@list1 = @{$_[0]};
+	@list2 = @{$_[1]};
+	
+	# make index of first occurrence of word in @list1 - eg $index{"Raku"} == 1
+	for $j (0 .. scalar @list1 - 1) {
+		$index{$list1[$j]} = $j unless $index{$list1[$j]};
+	}
+	
+	# scan through @list2
+	$least = 10^6;
+	$rubric1 = '';
+	for $index2 (0 .. scalar @list2 - 1) {
+		last if $index2 > $least;
+		$index1 = $index{$list2[$index2]};
+		
+		# is this $list2 word in @list1?
+		if (defined $index1) {
+			$sum = $index1 + $index2;
+			next if $sum > $least;
+			
+			# found a lesser sum of indices
+			$rubric1 = '' if $sum < $least;
+			$least = $sum;
+			$rubric1 .= qq["$list2[$index2]", ];
+		}
+	}
+	
+	# show the answers
+	say qq[\nInput:  \@list1 = ("] . join('", "', @list1) . q[")];
+	say qq[        \@list2 = ("] . join('", "', @list2) . q[")];
+	say qq[Output: (] . substr($rubric1, 0, -2) . q[)];	
+}	
+
---
+++ 208/2/bruce-gray.raku
@@ -0,0 +1,21 @@
+#!/usr/bin/env raku
+sub task2 ( @ns --> List ) {
+    my @repeated = @ns.repeated;
+    my @missing  = ( (1 .. +@ns) (^) @ns ).keys.sort(+*);
+
+    return ( @repeated == 1 and @missing == 1 )
+        ?? ( @repeated.head,    @missing.head )
+        !! Nil;
+}
+
+
+constant @tests =
+    ( (1,2,2,4) , (2,3) ),
+    ( (1,2,3,4) , -1    ),
+    ( (1,2,3,3) , (3,4) ),
+;
+use Test;
+plan +@tests;
+for @tests -> ( $in, $expected ) {
+    is-deeply (task2($in) // -1), $expected;
+}
