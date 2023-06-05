26022886e14b2d237df9369729f0fc83b7bc1052
David Hoekman <sqrtneginf@gmail.com>
2023-06-05 06:39:35 -0700

    challenge 219 task 2: perl

--- 219/2/wlmb.pl
+++ 219/2/wlmb.pl
@@ -2,13 +2,15 @@
 use v5.36;
 
 use List::Util qw(min);
-die <<~"FIN" unless @ARGV > 3;
-    Usage: $0 C1 C7 C30 D1 [D2...]
-    to calculate the travel expenditure for days D1, D2...
-    given the costs C1 C7 and C30 for one, seven and thirty day passes
-    FIN
-my @kinds=(1,7,30); # kinds of passes
-my @days=@ARGV;
+
+#die <<~"FIN" unless @ARGV > 3;
+#    Usage: $0 C1 C7 C30 D1 [D2...]
+#    to calculate the travel expenditure for days D1, D2...
+#    given the costs C1 C7 and C30 for one, seven and thirty day passes
+#    FIN
+
+my @kinds=(2,7,25); # kinds of passes
+my @days=(1, 5, 6, 7, 9, 15);
 my @costs=splice @days, 0, 3;
 my $expenditure=cost(0, @days);
 say "Costs: @costs, Days: @days, Expenditure: $expenditure";
