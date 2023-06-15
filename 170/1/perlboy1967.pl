628a540f05bbbdb5ab3de4355bcf28afa27ab9d6
David Hoekman <sqrtneginf@gmail.com>
2023-06-15 14:56:37 -0700

    example with and without prototypes

--- 170/1/perlboy1967.pl
+++ 170/1/perlboy1967.pl
@@ -1,17 +1,33 @@
 #!/usr/bin/env perl
-#use v5.36; # runs, but no meaningly results
-use v5.16;  # sticking with this for now
+use v5.36; # example with and without prototypes
 
 use Math::Primality qw(next_prime);
 
 # Prototype(s)
-sub primorialNumber ($\@);
+sub primorialNumber :prototype($\@);
 
 my @p;
-printf("%d\t=> %d\t(%s)\n", $_, primorialNumber($_, @p), join(',',@p)) for (0..15);
+printf "%d\t=> %d\t(%s)\n", $_, primorialNumber($_, @p), join(',',@p) for 0..15;
+say '---';
+printf "%d\t=> %d\t(%s)\n", $_, primorialNumber_noproto($_, \@p), join(',',@p) for 0..15;
 
+sub primorialNumber :prototype($\@) {
+  my ($i,$j) = @_;
 
-sub primorialNumber ($\@) {
+  state $p = [1];
+  state $n = [1];
+
+  while (@$n <= $i) {
+    push(@$p, next_prime($p->[-1]));
+    push(@$n, $p->[-1] * $n->[-1]);
+  }
+
+  @$j = @$p[0 .. @$p-1];
+
+  return $n->[$i];
+}
+
+sub primorialNumber_noproto {
   my ($i,$j) = @_;
 
   state $p = [1];
