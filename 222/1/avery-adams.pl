f76aba4b7677fb1329919bda337cd7c1933916cd
David Hoekman <sqrtneginf@gmail.com>
2023-06-26 05:43:23 -0700

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 222/1/avery-adams.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+use strict;
+use v5.24;
+my @sorted = sort @ARGV;
+my $matches;
+for (my $i = 0; $i <= $#ARGV; $i++) {$matches++ if $ARGV[$i] == $sorted[$i]}
+say $matches // 0;
