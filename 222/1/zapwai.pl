43c265582a12d1d709ea083074194dc866aa6af5
David Hoekman <sqrtneginf@gmail.com>
2023-06-26 08:03:02 -0700

    challenge 222 task 1: perl

--- 222/1/zapwai.pl
+++ 222/1/zapwai.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/env perl
 use v5.30;
 
-my @ints1 = (1, 1, 4, 2, 1, 3);
 my @ints2 = (5, 1, 2, 3, 4);
-my @ints3 = (1, 2, 3, 4, 5);
+my @ints1 = (1, 2, 3, 4, 5);
+my @ints3 = (1, 1, 4, 2, 1, 3);
 my @integ = (\@ints1, \@ints2, \@ints3);
 for my $r (@integ) {
     my @ints = @$r;
