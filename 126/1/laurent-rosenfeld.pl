64a2497fa1abef8697bdafadcc290208a1dbb6e0
David Hoekman <sqrtneginf@gmail.com>
2023-06-27 04:45:04 -0700

    test v5.36 compatibility, clean-up: 126*

--- 126/1/laurent-rosenfeld.pl
+++ 126/1/laurent-rosenfeld.pl
@@ -1,10 +1,6 @@
 #!/usr/bin/env perl
 use v5.36;
 
-use strict;
-use warnings;
-use feature qw/say/;
-
 my $n = shift // 200;
 my $count = scalar grep {not /1/} 2..$n;
 say "There are $count integers with no 1 in the 1..$n range";
