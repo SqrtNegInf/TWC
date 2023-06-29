dd7e5add87d445fc14e705a2eeef3c7abdeb4ad8
David Hoekman <sqrtneginf@gmail.com>
2023-06-29 08:18:15 -0700

    test v5.36 compatibility, clean-up: 020*

--- 020/1/guillermo-ramos.pl
+++ 020/1/guillermo-ramos.pl
@@ -1,9 +1,6 @@
 #!/usr/bin/env perl
 use v5.36;
 
-use strict;
-use warnings;
-
 my @chunks;
 push @chunks, $& while 'bookkeeper' =~ /(.)\1*/g;
 print "@chunks\n";
--- 020/1/gustavo-chaves.pl
+++ 020/1/gustavo-chaves.pl
@@ -1,8 +1,4 @@
 #!/usr/bin/env perl
 use v5.36;
 
-use 5.026;
-use strict;
-use warnings;
-
 say $& while 'bookkeeper' =~ /(.)\g1*/g;
