808ec81f22638ca6fcc1ca71d02a21f8d724ca71
David Hoekman <sqrtneginf@gmail.com>
2023-06-29 08:18:14 -0700

    test v5.36 compatibility, clean-up: 041*

--- 041/1/javier-luque.pl
+++ 041/1/javier-luque.pl
@@ -1,10 +1,7 @@
 #!/usr/bin/env perl
 use v5.36;
-# Test: ./ch-1.pl
-use strict;
-use warnings;
+
 use Math::Prime::Util qw /factor is_prime/;
-use feature qw /say/;
 
 for my $i (1..50) {
     my @factors = factor($i);
