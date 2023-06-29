0a91d9cfe66bf9dc15681612f1db0a8ea04d83ee
David Hoekman <sqrtneginf@gmail.com>
2023-06-29 08:18:14 -0700

    test v5.36 compatibility, clean-up: 029*

--- 029/1/lubos-kolouch.pl
+++ 029/1/lubos-kolouch.pl
@@ -1,32 +1,6 @@
 #!/usr/bin/env perl
 use v5.36;
-#===============================================================================
-#
-#         FILE: ch-1.pl
-#
-#        USAGE: ./ch-1.pl  
-#
-#  DESCRIPTION: https://perlweeklychallenge.org/blog/perl-weekly-challenge-029/
-#
-#  Write a script to demonstrate brace expansion. For example, script would
-#  take command line argument Perl {Daily,Weekly,Monthly,Yearly} Challenge and
-#  should expand it and print like below:
-#
-#      OPTIONS: ---
-# REQUIREMENTS: ---
-#         BUGS: ---
-#        NOTES: ---
-#       AUTHOR: YOUR NAME (), 
-# ORGANIZATION: 
-#      VERSION: 1.0
-#      CREATED: 10/11/2019 03:56:17 PM
-#     REVISION: ---
-#===============================================================================
 
-use strict;
-use warnings;
-use feature qw/say/;
-#use Data::Dumper;
 use Text::Glob::Expand;
 
 sub expand_brace {
