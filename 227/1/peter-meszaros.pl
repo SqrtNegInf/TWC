89e92a8ffa51f778b5cce57ee422de4d84f19829
David Hoekman <sqrtneginf@gmail.com>
2023-07-31 12:16:13 -0700

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 227/1/peter-meszaros.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Date::Calc qw/Day_of_Week/;
+use Test::More;
+#use Data::Dumper;
+
+my $cases = [
+	1753,
+	2023,
+	9999,
+];
+
+sub friday13th
+{
+    my $y = shift;
+	my $c = 0;
+	for my $m (1..12) {
+		my $dow = Day_of_Week($y, $m, 13);
+		++$c if $dow == 5; # Friday
+	}
+	return $c;
+}
+
+is(friday13th($cases->[0]),    2, 'year 1753');
+is(friday13th($cases->[1]),    2, 'year 2023');
+is(friday13th($cases->[2]),    1, 'year 9999');
+done_testing();
+
+exit 0;
