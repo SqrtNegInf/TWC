45cccfe82230fcada2d4bbba5ebb266c17e7b9aa
David Hoekman <sqrtneginf@gmail.com>
2023-09-11 04:24:58 -0700

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 233/1/mauke.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use v5.36;
+use List::Util qw(uniqstr);
+
+sub normalize($word) {
+    join '', uniqstr sort split //, $word
+}
+
+my @words = @ARGV;
+
+my $similar = 0;
+
+my %seen;
+for my $word (@words) {
+    $similar += $seen{normalize $word}++;
+}
+
+say $similar;
---
+++ 233/2/mauke.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use v5.36;
+
+my @ints = @ARGV;
+
+my %seen;
+$seen{$_}++ for @ints;
+
+say for sort {
+    $seen{$a} <=> $seen{$b} ||
+    $b <=> $a
+} @ints;
