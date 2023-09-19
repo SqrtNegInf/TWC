6fdeb95890744294e904b0b4c3be3012c8a67ce5
David Hoekman <sqrtneginf@gmail.com>
2023-09-18 19:48:30 -0700

    test v5.36 compatibility, 234/2

--- 234/2/peter-meszaros.pl
+++ 234/2/peter-meszaros.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/env perl
-
-use strict;
-use warnings;
+use v5.36;
 
 use Test::More;
 use Algorithm::Combinatorics qw/combinations/;
--- 234/2/zapwai.pl
+++ 234/2/zapwai.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-use v5.30;
+use v5.36;
 
 my @ints = (4, 4, 2, 4, 3);
 
