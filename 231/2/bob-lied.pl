ac559c8c1f49ae975f979cd27439114081dcfc69
David Hoekman <sqrtneginf@gmail.com>
2023-09-19 03:15:19 -0700

    update for output stability

--- 231/2/bob-lied.pl
+++ 231/2/bob-lied.pl
@@ -21,13 +21,13 @@ sub sc_pack(@passenger)
 
 sub runTest
 {
-    use Test2::V0;
+    use Test::More;
 
-    is( seniorCitizen( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
-    is( seniorCitizen( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
+    is_deeply( seniorCitizen( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
+    is_deeply( seniorCitizen( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
 
-    is( sc_pack( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
-    is( sc_pack( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
+    is_deeply( sc_pack( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
+    is_deeply( sc_pack( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
 
     done_testing;
 }
