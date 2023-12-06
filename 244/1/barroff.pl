323a8c902e72fe5d5e066f7cefae2d73dc546925
David Hoekman <sqrtneginf@gmail.com>
2023-12-05 18:35:28 -0800

    update for output stability

--- 244/1/barroff.pl
+++ 244/1/barroff.pl
@@ -21,14 +21,14 @@ sub MAIN() {
     }
     else {
         #| Run test cases
-        use Test2::V0 qw( is plan );
-        plan 3;
+        use Test::More;
 
-        is count_smaller( 8, 1, 2, 2, 3 ), [ 4, 0, 1, 1, 3 ],
+        is_deeply count_smaller( 8, 1, 2, 2, 3 ), [ 4, 0, 1, 1, 3 ],
           'works for (8, 1, 2, 2, 3)';
-        is count_smaller( 6, 5, 4, 8 ), [ 2, 1, 0, 3 ],
+        is_deeply count_smaller( 6, 5, 4, 8 ), [ 2, 1, 0, 3 ],
           'works for (6, 5, 4, 8)';
-        is count_smaller( 2, 2, 2 ), [ 0, 0, 0 ], 'works for (2, 2, 2)';
+        is_deeply count_smaller( 2, 2, 2 ), [ 0, 0, 0 ], 'works for (2, 2, 2)';
+        done_testing;
     }
 }
 
