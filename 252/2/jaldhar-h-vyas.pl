7f33662e95e0b331e182734119d748efd5b2e2ff
David Hoekman <sqrtneginf@gmail.com>
2024-01-22 10:00:03 -0800

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 252/2/jaldhar-h-vyas.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use 5.030;
+use warnings;
+
+my $n = shift; 
+my @results = map { $_, -$_ } (1 .. $n / 2);
+
+unless ($n % 2 == 0) {
+    push @results, 0;
+}
+
+say q{(}, (join q{, }, sort @results), q{)};
