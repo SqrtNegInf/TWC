18f43fd323fcc7957f36c642898591ff912b6e1a
David Hoekman <sqrtneginf@gmail.com>
2024-02-12 03:55:15 -0800

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 255/1/kjetillll.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+sub odd_char {
+    my %toggle;
+    $toggle{$_}++ and delete $toggle{$_} for "@_ "=~/./gs;
+    (keys%toggle)[0];
+}
+
+use Test::More;
+is( odd_char("Perl", "Preel")     => 'e' );
+is( odd_char("Weekly", "Weeakly") => 'a' );
+is( odd_char("Box", "Boxy")       => 'y' );
+done_testing;
