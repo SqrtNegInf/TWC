899cbda84e91be4a8222bbad6af15c0f5f289d02
David Hoekman <sqrtneginf@gmail.com>
2024-04-15 13:04:37 -0700

    downgrade 5.38 to 5.36

--- 264/1/bob-lied.pl
+++ 264/1/bob-lied.pl
@@ -1,27 +1,6 @@
 #!/usr/bin/env perl
-# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
-#=============================================================================
-# Copyright (c) 2024, Bob Lied
-#=============================================================================
-#  
-# ch-1.pl Perl Weekly Challenge 264 Task 1 Greatest English Letter
-#=============================================================================
-# You are given a string, $str, made up of only alphabetic characters
-# [a..zA..Z].  Write a script to return the greatest english letter in
-# the given string.  A letter is greatest if it occurs as lower and upper
-# case. Also letter ‘b’ is greater than ‘a’ if ‘b’ appears after ‘a’ in
-# the English alphabet.
-# Example 1 Input: $str = 'PeRlwEeKLy'
-#           Output: L
-#   There are two letters E and L that appears as lower and upper.
-#   The letter L appears after E, so the L is the greatest english letter.
-# Example 2 Input: $str = 'ChaLlenge'
-#           Output: L
-# Example 3 Input: $str = 'The'
-#           Output: ''
-#=============================================================================
 
-use v5.38;
+use v5.36;
 
 use builtin qw/true false/; no warnings "experimental::builtin";
 
