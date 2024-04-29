3528eb92ee0bd18292e444d2fdaa8fbe368cfa9e
David Hoekman <sqrtneginf@gmail.com>
2024-04-29 16:26:35 -0700

    raw code after tweaks and 1st-pass purge of Perl

---
+++ 266/1/feng-chang.raku
@@ -0,0 +1,5 @@
+#!/usr/bin/env raku
+
+unit sub MAIN(Str:D $sent1, Str:D $sent2);
+
+put ($sent1, $sent2)».words.Slip.Bag.grep(*.value == 1)».key.sort;
---
+++ 266/1/james-smith.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+sub uncommon_words {
+    my %counts;
+    for (@_) {
+    	$counts{$_}++ for split /\W+/, lc
+    }
+    my @solo = grep { $counts{$_}==1 } keys %counts;
+    @solo ? @solo : '';
+}
